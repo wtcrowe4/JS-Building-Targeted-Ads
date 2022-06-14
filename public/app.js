7ba02d3335d596b00819da5666085e1b3484bd77
Thomas Crowe <wcrowe@g.clemson.edu>
2022-06-13 20:28:11 -0400

    Worked through activity.

--- public/app.js
+++ public/app.js
@@ -1,20 +1,51 @@
-// get user's data
-// get user's coordinates
+// Get the user's coordinates:                                                              
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
+//console.log(getCoords());                              
+
+// Get the user's time:                                                              
+function userTime(){
+    const now = new Date()
+    return now.getHours()
+}
+//console.log(userTime())                             
+
+//Check time of day:                                                             
+function getMealTime(){
+    const tod = userTime()
+    return tod > 20 ? 'latenight snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
+//console.log(getMealTime())                
+
+// Build Ad 1:                                                           
+function buildAd1(){
+    const mealTime = getMealTime()
+    let content = document.querySelector('.ad1')
+    let inner = document.createElement('p')
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`
+    content.append(inner)
+}
+
+// Build Ad 2:                                                             
+function buildAd2(coordinates){
+    const coords = coordinates
+    const href = `https://www.google.com/maps/search/coffee/@${coords[0]},${coords[1]},15z/`
+    let content = document.querySelector('.ad2')
+    let inner = document.createElement('p')
+    inner.innerHTML = `It's time to try our coffee! <span><a href="${href}" target="_blank">We're this close!</a></span>`
+    content.append(inner)
+}
+// console.log(buildAd2(getCoords()))
+
+// On load, build ads:                                                             
+window.onload = async () => {
+    buildAd1()
+    const coords = await getCoords()
+    buildAd2(coords)
+}
 
 
-// get user's time
-
-
-// helper functions
-// check time of day
-
-
-// build ads
-// build ad 1
-
-
-// build ad 2
-
-
-// event listeners
-// on load, build ads
